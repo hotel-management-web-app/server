7ca396f3d8ed83b6b59e7b79d94415786c0d663f
Jakub Zakowicz <63516552+JakubZakowicz@users.noreply.github.com>
2023-03-16 09:33:57 +0100

    #39 - hosting (#40)
    * #39-change migration
    
    * #39 - change package.json

--- prisma/migrations/20230227182303_hotel/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `totalPrice` on the `Booking` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "Booking" ALTER COLUMN "totalPrice" SET DATA TYPE INTEGER;
