a05b2a1f4b9bc81d332a95275f173626a2619724
JakubZakowicz <j.zakowicz99@gmail.com>
2023-05-07 13:07:26 +0200

    #51 - fix booking price bug

--- src/utils/countDaysBetweenDates.ts
+++ src/utils/countDaysBetweenDates.ts
@@ -1,5 +1,11 @@
-export const countDaysBetweenDates = (arrivalDate: Date, departureDate: Date) =>
-  Math.ceil(
+export const countDaysBetweenDates = (
+  arrivalDate: Date,
+  departureDate: Date,
+) => {
+  const daysBetweenDates = Math.ceil(
     (new Date(departureDate).getTime() - new Date(arrivalDate).getTime()) /
       (60 * 60 * 24 * 1000),
   );
+
+  return daysBetweenDates === 0 ? 1 : daysBetweenDates;
+};
