6130d29e3c18c2c747cc4656fc6c31c73b59deb9
JakubZakowicz <j.zakowicz99@gmail.com>
2023-05-07 22:25:49 +0200

    #54 - add confirmed booking email

--- src/controllers/checkoutController.ts
+++ src/controllers/checkoutController.ts
@@ -1,10 +1,12 @@
 import asyncHandler from 'express-async-handler';
+import nodemailer from 'nodemailer';
 import prisma from '../lib/prisma';
 import Stripe from 'stripe';
 import { countDaysBetweenDates } from '../utils/countDaysBetweenDates';
 import { createCustomError } from '../utils/error';
 import { bookingFormSchema } from '../lib/validationSchemas';
 import Validator from '../utils/validator';
+import { Booking, Guest } from '@prisma/client';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2022-11-15',
@@ -98,6 +100,8 @@ export const createNewBooking = asyncHandler(async (req, res, next) => {
     },
   });
 
+  sendEmailMessage(guest, newBooking);
+
   res.send({ guest, newBooking });
 });
 
@@ -158,4 +162,55 @@ const findAvailableRoom = async () => {
       return true;
     });
   });
+};
+
+const sendEmailMessage = async (guest: Guest, booking: Booking) => {
+  const user = process.env.CONTACT_USER;
+  const password = process.env.CONTACT_PASSWORD;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: user, // generated ethereal user
+      pass: password, // generated ethereal password
+    },
+  });
+
+  const room = await prisma.room.findUnique({
+    where: { id: booking.roomId },
+    include: { roomType: true },
+  });
+  const generalSettings = await prisma.generalSettings.findFirst();
+
+  const { firstName, lastName } = guest;
+  const { arrivalDate, departureDate, adults, children, totalPrice } = booking;
+  const nights = countDaysBetweenDates(arrivalDate, departureDate);
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: user, // sender address
+    to: guest.email, // list of receivers
+    subject: `Hotel - Booking confirmation`, // Subject line
+    html: `
+    <div>
+      <h1>Dear ${firstName} ${lastName}</h1>
+      <p>Thank you for your booking. This email confirms that your booking has been received.</p>
+      <br />
+      <h2>Details</h2>
+      <p><strong>Room type:</strong> ${room?.roomType.name}</p>
+      <p><strong>Check-in date:</strong> ${arrivalDate}</p>
+      <p><strong>Check-out date:</strong> ${departureDate}</p>
+      <p><strong>Number of nights:</strong> ${nights}</p>
+      <p><strong>Adults:</strong> ${adults}</p>
+      <p><strong>Children:</strong> ${children}</p>
+      <p><strong>Price:</strong> ${(totalPrice / 100).toFixed(2)}</p>
+      <br />
+      <p>We look forward to seeing you there! Have a happy stay!</p>
+      <br />
+      <p>Best Regards</p>
+      <p><strong>${generalSettings?.hotelName}</strong></p>
+    </div>`, // html body
+  });
+
+  return info;
 };
