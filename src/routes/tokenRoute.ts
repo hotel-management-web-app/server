aa83af95220bb5d18c358bbf201832d2e207e5d8
JakubZakowicz <j.zakowicz99@gmail.com>
2023-05-13 16:13:36 +0200

    #59 - add token docs

--- src/routes/tokenRoute.ts
+++ src/routes/tokenRoute.ts
@@ -3,6 +3,21 @@ import { getCSRFToken, getJWT } from '../controllers/tokenController';
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * '/api/jwt':
+ *  get:
+ *    tags:
+ *    - JWT token
+ *    summary: Get JWT token
+ *    description: Get JWT token
+ *    responses:
+ *      200:
+ *        description: Success
+ *      404:
+ *        description: Bad Request
+ */
+
 router.get('/jwt', getJWT);
 router.get('/csrf-token', getCSRFToken);
 
